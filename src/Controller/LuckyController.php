fa6bbd1497b4075cb012cb846ca4082a24d95061
alexandre-pinon <alexandre.pinon@epitech.eu>
2021-04-12 17:34:15 +0200

    Moar packages, db init

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,16 +1,19 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-
-class LuckyController
+use Symfony\Component\Routing\Annotation\Route;
+class LuckyController extends AbstractController
 {
+    #[Route('/lucky/number', name: 'lucky_number')]
     public function number(): Response
     {
         $number = random_int(0, 100);
+        $url = $this->generateUrl('lucky_number', ['max' => 10]);
 
         return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
+            '<html><body>Lucky number: '.$number.'<br>URL: '.$url.'</body></html>'
         );
     }
 }
