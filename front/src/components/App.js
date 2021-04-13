1fb2e1a5b93ab38f2eb95f0a6eb8d318ed3507ac
Keisay <kinsay.soundcloud@gmail.com>
2021-04-13 23:01:35 +0200

    Front: Adding Routes for Authentification (Login, Register)

---
+++ front/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      E-Commerce
+    </div>
+  );
+}
+
+export default App;
--- front/src/index.js
+++ front/src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from "./components/App";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Switch>
+  </Router>
 );
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
