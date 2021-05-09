228539c13f73cf57bcebc62a12030847a586f166
Keisay <kinsay.soundcloud@gmail.com>
2021-05-09 11:50:47 +0200

    Some Front to Test API

--- client_ecommerce/src/components/misc/Navbar.js
+++ client_ecommerce/src/components/misc/Navbar.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { LogoIcon } from "./Icons";
 
 function Navbar() {
     return (
-        <div className="navbar">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <span className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+            <LogoIcon />
+            <span className="flex items-center justify-center ml-3 text-xl">
+                <span className="inline-block py-1 px-2 text-white text-2xl font-medium tracking-wider">
+            
+                </span>
             <Link to="/">
-                <h1>Snippet Manager</h1>
+                    React Shopper
             </Link>
-            <Link to="/login">Log in</Link>
-            <Link to="/register">Register</Link>
-        </div>
+            <Link to="/login" style={{ marginLeft: 10 }}>Login</Link>
+            <Link to="/register" style={{ marginLeft: 10 }}>Register</Link>
+          </span>
+        </span>
+        
+      </div>
+      
     );
 }
 
